873258600cf0870eb0af10eabaa8e936a2e0e05c
Madhukant Pandey <95975298+madhukantp97@users.noreply.github.com>
2025-06-02 18:54:45 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 // src/app/page.tsx
 "use client";
 
-import Layout from '@/components/Layout'; // Import your layout
 import DashboardContent from '@/components/Dashboard'; // Your dashboard specific content
 
 export default function RootPage() {
